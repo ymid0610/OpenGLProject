f1e13ed552dc89b3ae86cde3016fec5e14eeaed8
ymid0610 <ymid0610@gmail.com>
2025-09-23 01:02:35 +0900

    레전드 영목헤더 추가

---
+++ ymheader.h
@@ -0,0 +1,80 @@
+#pragma once
+#include <iostream>
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+
+GLfloat ConvertMouseToOpenGLX(int x, int windowWidth) {
+	return (2.0f * x) / windowWidth - 1.0f;
+}
+
+GLfloat ConvertMouseToOpenGLY(int y, int windowHeight) {
+	return 1.0f - (2.0f * y) / windowHeight;
+}
+
+GLfloat RandomValue() {
+	return -1.0f + ((rand() % 200) * 0.01f);
+}
+
+class YMRECT {
+public:
+	GLfloat x, y, size;
+	GLfloat red, green, blue;
+	GLfloat x1, y1, x2, y2;
+
+	YMRECT() {
+		random_color();
+	}
+	YMRECT(float ix, float iy, float isize) {
+		x = ix, y = iy; size = isize;
+		x1 = x - size; y1 = y - size; x2 = x + size; y2 = y + size;
+		random_color();
+	}
+	void random_color() {
+		red = 0.0f + ((rand() % 100) * 0.01f);
+		green = 0.0f + ((rand() % 100) * 0.01f);
+		blue = 0.0f + ((rand() % 100) * 0.01f);
+	}
+	bool is_mouse_inside(GLfloat px, GLfloat py) {
+		if (px >= x1 && px <= x2 && py >= y1 && py <= y2) return true;
+		else return false;
+	}
+	void remake_x1y1x2y2() {
+		x1 = x - size; y1 = y - size; x2 = x + size; y2 = y + size;
+	}
+	void remake_xy_size() {
+		x = (x1 + x2) / 2.0f;
+		y = (y1 + y2) / 2.0f;
+		size = (x2 - x1) / 2.0f;
+		if (size > (y2 - y1) / 2.0f) size = (y2 - y1) / 2.0f;
+	}
+	void size_up(int max) {
+		if (size >= max) return;
+		size += 0.05f;
+		remake_x1y1x2y2();
+	}
+	void size_down(int min) {
+		if (size <= min) return;
+		size -= 0.05f;
+		remake_x1y1x2y2();
+	}
+	void union_rect(YMRECT rect) {
+		x1 = (x1 < rect.x1) ? x1 : rect.x1;
+		y1 = (y1 < rect.y1) ? y1 : rect.y1;
+		x2 = (x2 > rect.x2) ? x2 : rect.x2;
+		y2 = (y2 > rect.y2) ? y2 : rect.y2;
+		remake_xy_size();
+	}
+	YMRECT devide_rect() {
+		int random = rand() % 5;
+		for (int i = 0; i < random + 1; i++) {
+			size_down(0.1f);
+		}
+		remake_x1y1x2y2();
+
+		YMRECT temp;
+		temp.x = x + size; temp.y = y + size; temp.size = size;
+		temp.remake_x1y1x2y2();
+		return temp;
+	}
+};
